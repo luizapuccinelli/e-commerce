547ed75a524ed3d6ac46238aa0073bbed7b7f8e3
luizakataoka <luiza.kataoka@bixtecnologia.com.br>
2022-03-20 15:52:29 -0300

    configure prok[je

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home(){
+  return(
+    <>
+      <Head>
+        <title> Brechó da Giuka </title>
+      </Head>
+    </>
+  )
+}
